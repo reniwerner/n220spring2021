aeda89e253339a67cc3c6ee7ebc54877700fb059
reniwerner <reniwern@iu.edu>
2021-01-28 18:35:52 -0500

    Person Comp Cat Update

--- labs/lab1/js/apps.js
+++ labs/lab1/js/apps.js
@@ -1,7 +1,11 @@
 function setup() {
+  var brown ="#a87532";
+  var black ="#453e38";
+  var pink ="#de85bf";
     createCanvas(400, 400);
-    
-    circle(100, 100, 60);
+    fill(brown);
+    circle(100, 100, 35);
+    fill(black);
     circle(90, 90, 5);
     circle(110, 90, 5);
     triangle(95, 100, 100, 95, 105, 100);
@@ -11,7 +15,9 @@ function setup() {
     line(90, 110, 98, 108);
     line(107, 108, 115, 113);
     line(107, 108, 115, 109);
+    fill(pink);
     ellipse(102, 113, 8, 10 );
+    fill(brown);
     ellipse(107, 185, 100, 115);
     bezier(148, 220, 180, 170, 170, 160, 190, 170);
 
