035b1a9eac341d918088c42626c94666ebe39c92
reniwerner <reniwern@iu.edu>
2021-05-03 19:47:37 -0400

    final

---
+++ labs/final/js/apps.js
@@ -0,0 +1,14 @@
+function setup() {
+createCanvas(800, 800);
+}
+
+function draw() {
+    background(220);
+    line(100, 400, 600, 400);
+    line(100, 200, 600, 200);
+    line(250, 70, 250, 500);
+    line(450, 70, 450, 500);
+
+}
+
+
